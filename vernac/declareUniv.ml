9c84dec04c96d536d4ccc35d4f7c356e5ba5eabe
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2023-06-30 10:55:48 +0200

    Add a "Check Constraint" vernacular command

--- vernac/declareUniv.ml
+++ vernac/declareUniv.ml
@@ -171,3 +171,13 @@ let do_constraint ~poly l =
         (UVars.Instance.empty,constraints)
     in
     Global.push_section_context uctx
+ 
+let check_constraint env sigma l =
+  let open Univ in
+  let constraints = List.fold_left (fun acc cst ->
+      let cst = Constrintern.interp_univ_constraint sigma cst in
+      Constraints.add cst acc)
+      Constraints.empty l
+  in
+  if Evd.check_constraints sigma constraints then ()
+  else CErrors.user_err (Pp.str"Constraints do not hold")
