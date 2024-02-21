f4b0ebd004579b9245016e6cd78ee989f62a0b96
loic-p <loic@pujet.fr>
2024-02-22 00:52:00 +0100

    Rebase the generation of axioms for inductive types

---
+++ pretyping/observational.mli
@@ -0,0 +1,38 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+open Names
+open Constr
+
+(** From an inductive type, generates a list of observational equalities
+    that *)
+
+val duplicate_context : (constr, constr) Context.Rel.pt -> Esubst.lift * (constr, constr) Context.Rel.pt
+
+val declare_inductive_obs_eqs : Names.MutInd.t -> unit
+
+val declare_inductive_casts : Names.MutInd.t -> unit
+
+val declare_inductive_rewrite_rules : Names.MutInd.t -> unit
+
+val declare_inductive_observational_data : Names.MutInd.t -> unit
+
+val declare_observational_equality :
+  (univs:UState.named_universes_entry
+   -> name:Id.t
+   -> Constr.t
+   -> Constant.t) ref
+
+val declare_observational_rewrite :
+  (univs:UState.named_universes_entry
+   -> name:Id.t
+   -> Constr.t
+   -> Constr.t
+   -> Constant.t) ref
