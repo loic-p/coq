da3f33048d411f665768ac83120c876bfb0313f2
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2024-04-15 18:03:14 +0200

    Better debug message for start of unification

--- pretyping/unification.ml
+++ pretyping/unification.ml
@@ -1217,7 +1217,8 @@ let rec unify_0_with_initial_metas (sigma,ms,es as subst : subst0) conv_at_top e
 
   debug_tactic_unification (fun () ->
       str "Starting unification:" ++ spc() ++
-      Termops.Internal.print_constr_env env sigma (fst m) ++ strbrk" ~= " ++
+      Termops.Internal.print_constr_env env sigma (fst m) ++
+      (match cv_pb with CONV -> strbrk" ~= " | CUMUL -> strbrk" ~<= ") ++
       Termops.Internal.print_constr_env env sigma (fst n));
 
   let opt = { at_top = conv_at_top; with_types = false; with_cs = true } in
